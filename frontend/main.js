3f6d911eba538f753f9b79d229150a09d0446669
Carlos Urquiza <webquiza+cloud@gmail.com>
2023-02-20 10:18:05 -0600

    edit starter js code for counter

--- frontend/main.js
+++ frontend/main.js
@@ -1,23 +1,23 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    getVisitCount();
+window.addEventListener("DOMContentLoaded", (event) => {
+  getVisitCount();
 });
 
-
-const localApi = 'http://localhost:7071/api/GetResumeCounter';
-const functionApi = 'https://getresumecounterfunctionapp.azurewebsites.net/api/GetResumeCounter?code=jp4rx3wuLEzaXvSFeBKaq08RGiVCwRtpAA1Cdxdc15c9ZGjM79tWjg=='; 
+//const localApi = '';
+const functionApi = "";
 
 const getVisitCount = () => {
-    let count = 30;
-    fetch(functionApi)
-    .then(response => {
-        return response.json()
+  let count = 30;
+  fetch(functionApi)
+    .then((response) => {
+      return response.json();
     })
-    .then(response => {
-        console.log("Website called function API.");
-        count = response.count;
-        document.getElementById('counter').innerText = count;
-    }).catch(function(error) {
-        console.log(error);
-      });
-    return count;
-}
+    .then((response) => {
+      console.log("Site called function API.");
+      count = response.count;
+      document.getElementById("counter").innerText = count;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  return count;
+};
